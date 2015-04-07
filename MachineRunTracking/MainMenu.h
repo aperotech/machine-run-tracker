3893cae4eb7499f8641aa7dd6610cb6540234256
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-07 19:17:46 +0530

    done

---
+++ MachineRunTracking/MainMenu.h
@@ -0,0 +1,13 @@
+//
+//  MainMenu.h
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 07/04/15.
+//  Copyright (c) 2015 AperoTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenu : UIViewController
+
+@end
---
+++ MachineRunTracking/MainMenu.m
@@ -0,0 +1,13 @@
+//
+//  MainMenu.m
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 07/04/15.
+//  Copyright (c) 2015 AperoTechnologies. All rights reserved.
+//
+
+#import "MainMenu.h"
+
+@implementation MainMenu
+
+@end

d6649067e73947835f460f46c5dfb0071992e0c0
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-07 19:18:35 +0530

    done

---
+++ MachineRunTracking/User.h
@@ -0,0 +1,16 @@
+//
+//  User.h
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 07/04/15.
+//  Copyright (c) 2015 AperoTechnologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+@property (nonatomic,strong) NSString *Name;
+@property (nonatomic,strong) NSString *Password;
+@property (nonatomic,strong) NSString *User_type;
+@property (nonatomic,strong) NSString *User_Email;
+@end

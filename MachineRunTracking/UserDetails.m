cb7bad098fb2eab87dee1d20b124a66c1aaa4077
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-07 19:18:14 +0530

    done

---
+++ MachineRunTracking/UserDetails.m
@@ -0,0 +1,13 @@
+//
+//  UserDetails.m
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 07/04/15.
+//  Copyright (c) 2015 AperoTechnologies. All rights reserved.
+//
+
+#import "UserDetails.h"
+
+@implementation UserDetails
+
+@end

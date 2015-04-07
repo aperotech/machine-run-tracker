db817f58450521846513ff7af59642fb9b409bfa
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-07 19:18:42 +0530

    done

---
+++ MachineRunTracking/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 07/04/15.
+//  Copyright (c) 2015 AperoTechnologies. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+@synthesize Name,User_Email,User_type,Password;
+@end

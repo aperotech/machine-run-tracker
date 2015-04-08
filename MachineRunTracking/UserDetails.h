bb808dd6be58951acb47589712d9328c3484ef5d
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-08 17:51:50 +0530

    done

--- MachineRunTracking/UserDetails.h
+++ MachineRunTracking/UserDetails.h
@@ -7,7 +7,16 @@
 //
 
 #import "ViewController.h"
-
+#import <Parse/Parse.h>
 @interface UserDetails : ViewController
+@property (nonatomic, strong) PFObject *UpdateObjPF;
+@property (weak, nonatomic) IBOutlet UITextField *userNameUpdateText;
+@property (weak, nonatomic) IBOutlet UITextField *userTypeUpdateText;
+@property (weak, nonatomic) IBOutlet UITextField *userEmailUpdateText;
+@property (weak, nonatomic) IBOutlet UITextField *OldPassText;
+@property (weak, nonatomic) IBOutlet UITextField *NewPassText;
+@property (weak, nonatomic) IBOutlet UITextField *ReTypePassText;
+@property (weak, nonatomic) IBOutlet UIButton *UpdateButton;
 
+-(IBAction)UpdateButton:(id)sender;
 @end
--- MachineRunTracking/UserDetails.m
+++ MachineRunTracking/UserDetails.m
@@ -9,5 +9,101 @@
 #import "UserDetails.h"
 
 @implementation UserDetails
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Check to see if note is not nil, which let's us know that the note
+    // had already been saved.
+    if (self.UpdateObjPF != nil) {
+        self.userNameUpdateText.text = [self.UpdateObjPF objectForKey:@"username"];
+        self.userEmailUpdateText.text = [self.UpdateObjPF objectForKey:@"email"];
+        self.OldPassText.text=[self.UpdateObjPF objectForKey:@"password"];
+    }
+}
+
+- (IBAction)UpdateButton:(id)sender {
+    
+    NSString *title = [self.userNameUpdateText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+     NSString *email = [self.userEmailUpdateText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+     NSString *pass = [self.OldPassText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    if ([title length] == 0 ||[email length] == 0 ||[pass length] == 0) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                            message:@"You must at least enter a username"
+                                                           delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+    }
+    else {
+        
+        if (self.UpdateObjPF != nil) {
+            [self updateNote];
+        }
+        else {
+            [self saveNote];
+        }
+    }
+    
+}
+
+- (void)saveNote
+{
+    
+    PFObject *NewUser = [PFObject objectWithClassName:@"User"];
+    NewUser[@"username"] = self.userNameUpdateText.text;
+    NewUser[@"email"] = self.userEmailUpdateText.text;
+    NewUser[@"password"] = self.OldPassText.text;
+    NewUser[@"User"] = [PFUser currentUser];
+    
+    [NewUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            [self.navigationController popViewControllerAnimated:YES];
+        } else {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                                message:[error.userInfo objectForKey:@"error"]
+                                                               delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
+    }];
+    
+}
+
+- (void)updateNote
+{
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"User"];
+    
+    // Retrieve the object by id
+    [query getObjectInBackgroundWithId:[self.UpdateObjPF objectId] block:^(PFObject *UpdateUser, NSError *error) {
+        
+        if (error) {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                                message:[error.userInfo objectForKey:@"error"]
+                                                               delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
+        else {
+            UpdateUser[@"username"] = self.userNameUpdateText.text;
+            UpdateUser[@"email"] = self.userEmailUpdateText.text;
+            UpdateUser[@"password"] = self.OldPassText.text;
+
+            
+            [UpdateUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    [self.navigationController popViewControllerAnimated:YES];
+                } else {
+                    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                                        message:[error.userInfo objectForKey:@"error"]
+                                                                       delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [alertView show];
+                }
+            }];
+        }
+        
+    }];
+    
+}
+
+
 
 @end

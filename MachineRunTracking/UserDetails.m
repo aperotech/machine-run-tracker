9ebc30c9cdf42e8a4ba2b655528b5f5f721ce834
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-10 15:22:29 +0530

    done

--- MachineRunTracking/UserDetails.m
+++ MachineRunTracking/UserDetails.m
@@ -7,7 +7,7 @@
 //
 
 #import "UserDetails.h"
-
+#define k_KEYBOARD_OFFSET 80.0
 @implementation UserDetails
 - (void)viewDidLoad
 {
@@ -129,7 +129,7 @@
     return YES;
 }
 
-#pragma mark- Keyboard text field move up
+/*#pragma mark- Keyboard text field move up
 - (void)registerForKeyboardNotifications {
     
     
@@ -267,6 +267,30 @@
     
     
     
+}
+*/
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [self animateTextField:textField up:YES];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [self animateTextField:textField up:NO];
+}
+
+-(void)animateTextField:(UITextField*)textField up:(BOOL)up
+{
+    const int movementDistance = -60; // tweak as needed
+    const float movementDuration = 0.3f; // tweak as needed
+    
+    int movement = (up ? movementDistance : -movementDistance);
+    
+    [UIView beginAnimations: @"animateTextField" context: nil];
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    [UIView setAnimationDuration: movementDuration];
+    self.view.frame = CGRectOffset(self.view.frame, 0, movement);
+    [UIView commitAnimations];
 }
 
 

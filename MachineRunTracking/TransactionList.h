8196023cff9366680b19034387c4db028bf4fa48
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-13 18:37:36 +0530

    done

---
+++ MachineRunTracking/TransactionList.h
@@ -0,0 +1,13 @@
+//
+//  TransactionList.h
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 13/04/15.
+//  Copyright (c) 2015 AperoTechnologies. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+
+@interface TransactionList : PFQueryTableViewController
+
+@end
---
+++ MachineRunTracking/TransactionListCell.m
@@ -0,0 +1,23 @@
+//
+//  TransactionListCell.m
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 13/04/15.
+//  Copyright (c) 2015 AperoTechnologies. All rights reserved.
+//
+
+#import "TransactionListCell.h"
+
+@implementation TransactionListCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

1e566a0a3675236990ee6b29bef4eedd8794dd52
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-14 18:39:49 +0530

    done

--- MachineRunTracking/MainMenu.m
+++ MachineRunTracking/MainMenu.m
@@ -11,7 +11,31 @@
 @implementation MainMenu
 - (void)viewDidLoad {
     [super viewDidLoad];
+  /*  PFObject *dynamicclassobj=[PFObject  objectWithClassName:@"CustomClass" ];
     
+    if([dynamicclassobj save]) {
+       NSLog(@"Successfully Created");
+        PFObject *gameScore = [PFObject objectWithClassName:@"CustomClass"];
+        gameScore[@"score"] = @1337;
+         gameScore[@"myNumber"] = @"someValue";
+        gameScore[@"ScoreName"] = @123123;
+    gameScore[@"First_Name"] = @"Akshay";
+    gameScore[@"Last_Name"] = @"Shrirao";
+    gameScore[@"Nick_Name"] = @"Bunty";
+    gameScore[@"Company_Name"] = @"Apero Technologies";
+        [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (succeeded) {
+                NSLog(@"The object has been saved");
+                // The object has been saved.
+            } else {
+                NSLog(@"here was a problem, check error.description");
+                // There was a problem, check error.description
+            }
+        }];
+       
+      //  class created;
+    
+    }*/
    }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];

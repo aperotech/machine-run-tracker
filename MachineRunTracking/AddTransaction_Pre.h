b9dc8917a1546b25cb6170e2b3ae837186f006cb
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-17 19:11:05 +0530

    created an new parse class and getting values from dynamically created text fields

--- MachineRunTracking/AddTransaction_Pre.h
+++ MachineRunTracking/AddTransaction_Pre.h
@@ -7,18 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SegmentedLocationVC.h"
+#import <Parse/Parse.h>
+/*@class AddTransaction_Pre;
+@protocol AddTransaction_PreDelegates <NSObject>
+
+- (void)addPrameterViewController:(AddTransaction_Pre *)controller didFinishEnteringItem:(NSString *)Pre_String;
+@end
+*/
+
+
 
 @interface AddTransaction_Pre : UIViewController<UITextFieldDelegate>
 {
     BOOL Pre_extractionFlag;
 }
-//@property(strong,nonatomic)IBOutlet UITableView *tableView;
-@property (weak,nonatomic)IBOutlet UISegmentedControl *segmentedControl;
-@property (strong,nonatomic) NSArray *ViewControllers;
-@property (nonatomic,strong)NSDictionary *myDictionary;
-//@property (weak, nonatomic) IBOutlet UITextField *parameterText;
-@property (strong,nonatomic) IBOutlet UILabel *Pre_ExtractionLabel;
-
+@property(strong,nonatomic)PFObject *parameterAdd_PrePF;
+//@property (nonatomic, weak) id <AddTransaction_PreDelegates> delegate;
+//@property (weak,nonatomic) SegmentedLocationVC *SegmentedLocationVCObj;
+@property (weak, nonatomic) IBOutlet UIButton *Add;
+@property (strong,nonatomic) NSMutableArray *pre_extractionArray;
 @property(nonatomic)NSInteger ObjectCount;
-
+@property(nonatomic,strong)IBOutlet UITableView *tableView;
+@property(nonatomic,strong)NSString *Parameter0;
+@property(nonatomic,strong)NSString *Parameter1;
+@property(nonatomic,strong)NSString *Parameter2;
 @end
--- MachineRunTracking/AddTransaction_Pre.m
+++ MachineRunTracking/AddTransaction_Pre.m
@@ -8,15 +8,19 @@
 
 #import "AddTransaction_Pre.h"
 //#import "AddTransaction_PreCell.h"
-//#import "AddTransaction_Run.h"
+#import "AddTransaction_Run.h"
 //#import "AddTransaction_Post.h"
+//#import "SegmentedLocationVC.h"
+#import "SegmentedLocationVC.h"
 #import <Parse/Parse.h>
 @interface AddTransaction_Pre ()
-
+@property(strong,nonatomic)SegmentedLocationVC *SegmentedLocationVCObj;
+@property(strong,nonatomic)AddTransaction_Run *AddTransaction_RunObj;
 @end
 
 @implementation AddTransaction_Pre
-@synthesize Pre_ExtractionLabel;
+@synthesize tableView,parameterAdd_PrePF;
+//@synthesize SegmentedLocationVCObj;
 - (void)viewDidLoad {
     [super viewDidLoad];
    // [self setupViewControllers];
@@ -24,10 +28,12 @@
     self.navigationController.navigationBar.hidden=NO;
     self.navigationController.navigationItem.title=@"Pre";
     Pre_extractionFlag=0;
-     NSLog(@"AddTransaction_Pre Loaded!");
+   // self.AddTransaction_Pre = [self.storyboard instantiateViewControllerWithIdentifier:@"AddTransaction_Pre_ID"];
+    //self.AddTransaction_Pre.delegate  = self;
     // Do any additional setup after loading the view.
    // PFObject *transactionObj=[PFObject objectWithClassName:@"Transaction"];
-    
+   self.SegmentedLocationVCObj=[[SegmentedLocationVC alloc]init];
+    self.AddTransaction_RunObj=[[AddTransaction_Run alloc]init];
     PFQuery *query = [PFQuery queryWithClassName:@"Parameters"];
     [query whereKey:@"Type" equalTo:@"Pre_Extraction"];
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
@@ -41,13 +47,13 @@
                 NSLog(@"None found");
             }
             else {
+                
              //   NSLog(@"objectArray %@",objects);
             }
             
         }
     }];
-
-}
+   }
 - (IBAction)Cancel:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
 }
@@ -63,7 +69,7 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *simpleTableIdentifier = @"Pre_ExtractionCellIdentifier";
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
     if (cell != nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
        // cell.p_1Text.text = @"New Parameter";
@@ -74,18 +80,17 @@
         valueTextField.delegate = self;
         valueTextField.placeholder=@"Parameters";
         [cell.contentView addSubview:valueTextField];
-       // [valueTextField release];
-       // cell.
+       
     }
    // cell.p_1Text.text = @"New Parameter";
-  /*  for (int i=0; i<self.ObjectCount;i++) {
+    for (int i=0; i<self.ObjectCount;i++) {
         if (indexPath.row == i) {   
             
-            UITextField *parameterTextField = (UITextField *)[cell viewWithTag:1];
-            parameterTextField.placeholder = @"Parameter";
+            UITextField *parameterTextField = (UITextField *)[cell viewWithTag:i];
+            [self.pre_extractionArray addObject: parameterTextField.text];
         }
-        
-    }*/
+         NSLog(@"PreExtraction Array Is %@",self.pre_extractionArray);
+    }
         return cell;
 }
 
@@ -99,11 +104,228 @@ UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     UITableViewCell *cell = (UITableViewCell *)[[textField superview] superview];
     UITableView *table = (UITableView *)[[cell superview] superview];
     NSIndexPath *textFieldIndexPath = [table indexPathForCell:cell];
+     self.pre_extractionArray = [[NSMutableArray alloc] init];
+    if (textField.tag==0) {
+        self.Parameter0=textField.text;
+        NSLog(@"Parameter0 %@",self.Parameter0);
+  self.AddTransaction_RunObj.Parameter0=self.Parameter0;
+     //   [self.delegate addPrameterViewController:self didFinishEnteringItem:self.Parameter0];
+       //  [self.pre_extractionArray addObject:self.Parameter0];
+    }
+    if (textField.tag==1) {
+       self.Parameter1=textField.text;
+         NSLog(@"Parameter1 %@",self.Parameter1);
+      
+      self.AddTransaction_RunObj.Parameter1=self.Parameter1;
+       //  [self.pre_extractionArray addObject:self.Parameter1];
+   //     [self.delegate addPrameterViewController:self didFinishEnteringItem:self.Parameter1];
+    }
+    if (textField.tag==2) {
+        self.Parameter2=textField.text;
+         NSLog(@"Parameter2 %@",self.Parameter2);
+      self.AddTransaction_RunObj.Parameter2=self.Parameter2;
+     //    [self.pre_extractionArray addObject: self.Parameter2];
+    //    [self.delegate addPrameterViewController:self didFinishEnteringItem:self.Parameter2];
+    }
     
+  
     
-    NSLog(@"Row %ld just finished editing with the value %@  tag is %ld",(long)textFieldIndexPath.row,textField.text ,(long)textField.tag);
+       NSLog(@"Row %ld just finished editing with the value %@  tag is %ld",(long)textFieldIndexPath.row,textField.text ,(long)textField.tag);
+   }
 
+
+/*-(void) textFieldDidEndEditing:(UITextField *)textField
+{
+    // assuming your text field is embedded directly into the table view
+    // cell and not into any other subview of the table cell
+    UITableViewCell * parentView = (UITableViewCell *)[textField superview];
+    
+    if(parentView)
+    {
+        self.pre_extractionArray = [[NSMutableArray alloc] init];
+        NSString *cellOneTexfieldOneTxt;
+        
+        NSArray * allSubviews = [parentView subviews];
+        for(UIView * oneSubview in allSubviews)
+        {
+            // get only the text fields
+            if([oneSubview isKindOfClass: [UITextField class]])
+            {
+                UITextField * oneTextField = (UITextField *) oneSubview;
+                
+                if(oneTextField.text)
+                {
+                    [self.pre_extractionArray addObject: oneTextField.text];
+                } else {
+                    // if nothing is in the text field, should
+                    // we simply add the empty string to the array?
+                    [self.pre_extractionArray addObject: @""];
+                }
+            }
+            NSLog(@"End Editing---------- %@",self.pre_extractionArray);
+        }
+        
+    }
+    
+    // don't forget to actually *DO* something with your mutable array
+    // (and release it, in case you're not using ARC), before this method
+    // returns.
+}*/
+
+
+-(IBAction)AddBtnClck:(id)sender {
+    
+   /* NSString *name = [nameText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    NSString *description = [descriptionText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *type = [typeText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *Units = [unitsText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    
+    if ([name length] == 0 ||[description length] == 0 ||[type length] == 0 ||[Units length] == 0) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                            message:@"You must enter details"
+                                                           delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+    }
+    else {*/
+        
+        
+        
+        if (parameterAdd_PrePF != nil) {
+            [self updateParameters];
+        }
+        else {
+            [self saveParameters];
+        }
+    //}
+    
 }
+
+- (void)saveParameters
+{
+      PFObject *NewParameter=[PFObject  objectWithClassName:@"Pre_Extraction" ];
+     
+     if([NewParameter save]) {
+     NSLog(@"Successfully Created");
+     PFObject *ParameterValue = [PFObject objectWithClassName:@"Pre_Extraction"];
+     ParameterValue[@"Parameter_1"] = self.Parameter0;
+     ParameterValue[@"Parameter_2"] = self.Parameter1;
+     ParameterValue[@"Parameter_3"] = self.Parameter2;
+     ParameterValue[@"Parameter_4"] = @"Akshay";
+     
+     [ParameterValue saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+     if (succeeded) {
+     [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshTable" object:self];
+    // [self.navigationController popViewControllerAnimated:YES];
+     NSLog(@"The object has been saved");
+     // The object has been saved.
+     } else {
+     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+     message:[error.userInfo objectForKey:@"error"]
+     delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+     [alertView show];
+
+     NSLog(@"here was a problem, check error.description");
+     // There was a problem, check error.description
+     }
+     }];
+     
+     //  class created;
+     
+     }
+   /* PFObject *NewParameter = [PFObject objectWithClassName:@"Pre_Extraction"];
+    
+    
+    [NewParameter setObject:self.Parameter0 forKey:@"Parameter_1"];
+    [NewParameter setObject:self.Parameter1 forKey:@"Parameter_2"];
+    [NewParameter setObject:self.Parameter2 forKey:@"Parameter_3"];
+    [NewParameter setObject:self.Parameter0 forKey:@"Parameter_4"];
+    
+    
+    
+    
+    //  NewMachine[@"Machine"] = [PFUser currentUser];
+    
+    [NewParameter saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshTable" object:self];
+            [self.navigationController popViewControllerAnimated:YES];
+        } else {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                                message:[error.userInfo objectForKey:@"error"]
+                                                               delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
+    }];
+    */
+}
+
+- (void)updateParameters
+{
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"Pre_Extraction"];
+    
+    // Retrieve the object by id
+    [query getObjectInBackgroundWithId:[parameterAdd_PrePF objectId] block:^(PFObject *UpdateParameter, NSError *error) {
+        
+        if (error) {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                                message:[error.userInfo objectForKey:@"error"]
+                                                               delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
+        else {
+            
+            [UpdateParameter setObject:self.Parameter0 forKey:@"Parameter_1"];
+            [UpdateParameter setObject:self.Parameter1 forKey:@"Parameter_2"];
+            [UpdateParameter setObject:self.Parameter2 forKey:@"Parameter_3"];
+            [UpdateParameter setObject:self.Parameter0 forKey:@"Parameter_4"];
+            
+            
+            [UpdateParameter saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshTable" object:self];
+                    [self.navigationController popViewControllerAnimated:YES];
+                } else {
+                    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                                        message:[error.userInfo objectForKey:@"error"]
+                                                                       delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [alertView show];
+                }
+            }];
+        }
+        
+    }];
+    
+}
+#pragma mark - UITextFieldDelegate method implementation
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+
+
+
+
+/*-(IBAction)AddBtnClck:(id)sender{
+    NSLog(@"The Strings Are %@ --- %@ ---- %@",self.Parameter0,self.Parameter1,self.Parameter2);
+    self.pre_extractionArray = [[NSMutableArray alloc] init];
+    [self.pre_extractionArray addObject: self.Parameter0];
+    [self.pre_extractionArray addObject: self.Parameter1];
+    [self.pre_extractionArray addObject: self.Parameter2];
+ NSLog(@"self.pre_extractionArray!!!!!! ---> %@",self.pre_extractionArray);
+}*/
+/*-(NSMutableArray *)newItem{
+    NSMutableArray *newItem=[[NSMutableArray alloc ]init];
+    for (int i=0;i<self.ObjectCount; i++) {
+        <#statements#>
+    }
+
+}*/
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.

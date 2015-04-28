fbfd18ae1468c98b995dc7481bd2272c22674b60
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-28 13:13:21 +0530

    Passed the PFobject for selected transaction to view controllers.

--- MachineRunTracking/SegmentedControlVC.h
+++ MachineRunTracking/SegmentedControlVC.h
@@ -7,11 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "ContainerViewController.h"
+#import <Parse/Parse.h>
 @interface SegmentedControlVC : UIViewController
 @property (strong,nonatomic) NSArray *allViewControllers;
 @property (nonatomic, strong) UIViewController *currentViewController;
+
 @property (weak,nonatomic)IBOutlet UISegmentedControl *segmentedControl;
 @property(strong,nonatomic) NSMutableArray *preExtractionArraySegment;
+
+@property(strong,nonatomic)PFObject *SegmentControlPF;
+@property (nonatomic, weak) ContainerViewController *containerView;
+
+- (IBAction)segmentControlChanged:(id)sender;
+
 
 @end

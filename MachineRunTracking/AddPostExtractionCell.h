80853a00958dfca587a1e7a742345cdced92aca2
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-04-28 17:48:17 +0530

    Added an PlaceHolder with parameter name in text fields

---
+++ MachineRunTracking/AddPostExtractionCell.h
@@ -0,0 +1,13 @@
+//
+//  AddPostExtractionCell.h
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 28/04/15.
+//  Copyright (c) 2015 Apero Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPostExtractionCell : UITableViewCell
+@property(strong,nonatomic)IBOutlet UITextField *p_1Text;
+@end
---
+++ MachineRunTracking/AddPostExtractionCell.m
@@ -0,0 +1,23 @@
+//
+//  AddPostExtractionCell.m
+//  MachineRunTracking
+//
+//  Created by Akshay Shrirao on 28/04/15.
+//  Copyright (c) 2015 Apero Technologies. All rights reserved.
+//
+
+#import "AddPostExtractionCell.h"
+
+@implementation AddPostExtractionCell
+@synthesize p_1Text;
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

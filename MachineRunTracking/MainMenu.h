fef763750ab0886d279369a24bd60ce9961f096e
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-05-04 17:35:00 +0530

    Added an Standard user Functionality to see only his details and admin to all

--- MachineRunTracking/MainMenu.h
+++ MachineRunTracking/MainMenu.h
@@ -7,12 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <Parse/Parse.h>
 @interface MainMenu : UIViewController
 - (IBAction)logout:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *UserButton;
 @property (weak, nonatomic) IBOutlet UIButton *MachineButton;
 @property (weak, nonatomic) IBOutlet UIButton *TransactionsButton;
 @property (weak, nonatomic) IBOutlet UIButton *ParametersButton;
-
+@property BOOL *PermissionFlag;
+@property (nonatomic, strong) PFObject *MainMenuObjPF;
+@property (nonatomic, strong) PFUser * CurrentUser;
+-(IBAction)UserButtonClick:(id)sender;
 @end
--- MachineRunTracking/MainMenu.m
+++ MachineRunTracking/MainMenu.m
@@ -9,12 +9,15 @@
 #import "MainMenu.h"
 #import <Parse/Parse.h>
 #import <QuartzCore/QuartzCore.h>
+#import "UserDetails.h"
+#import "UserList.h"
 @implementation MainMenu
 
 @synthesize UserButton,ParametersButton,TransactionsButton,MachineButton;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationItem.rightBarButtonItem.enabled=YES;
     UserButton.layer.borderWidth=1.0f;
     UserButton.layer.borderColor=[[UIColor blackColor]CGColor];
     ParametersButton.layer.borderWidth=1.0f;
@@ -24,17 +27,77 @@
     MachineButton.layer.borderWidth=1.0f;
     MachineButton.layer.borderColor=[[UIColor blackColor]CGColor];
     
+   
+   // StandardUserMainMenuToUserDetailsSegue
+    
+    
+    
+    
+    
      }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)logout:(id)sender {
+     PFUser *currentUser = [PFUser currentUser];
     [PFUser logOut];
-   // PFUser *currentUser = [PFUser currentUser]; // this will now be nil
-     //[self performSegueWithIdentifier:@"unwindToLoginSegue" sender:self];
-    [self dismissViewControllerAnimated:YES completion:nil];
+   // [self.navigationController popViewControllerAnimated:YES];
+    // this will now be nil
+     [self performSegueWithIdentifier:@"unwindToLoginSegue" sender:self];
+    //[self dismissViewControllerAnimated:YES completion:nil];
    // [self.navigationController popViewControllerAnimated:YES];
     //NSLog(@"Successfully Logout ");
     }
+
+-(IBAction)UserButtonClick:(id)sender{
+    [[PFUser currentUser] fetchInBackgroundWithBlock:nil];
+    self.CurrentUser = [PFUser currentUser];
+    
+    PFQuery *query = [PFUser query];
+    [query whereKey:@"username" equalTo:[[PFUser currentUser]username]];
+    [query whereKey:@"usertype" equalTo:@"Standard"];
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error){
+        if (!object) {
+            
+            self.PermissionFlag = TRUE;
+            NSLog(@"The Transaction Currenet User Is %@ ",object );
+            [self performSegueWithIdentifier:@"MainMenuToUserListSegue" sender:self];
+            // Did not find any UserStats for the current user
+        } else {
+           
+            self.MainMenuObjPF=object;
+            self.navigationItem.rightBarButtonItem.enabled=FALSE;
+            self.PermissionFlag = FALSE;
+            [self performSegueWithIdentifier:@"StandardUserMainMenuToUserDetailsSegue" sender:self];
+         
+        }
+    }];
+
+
+}
+
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+     
+     if ([segue.identifier isEqualToString:@"StandardUserMainMenuToUserDetailsSegue"]) {
+        
+         
+         UserDetails *userdetailsObj = (UserDetails *)segue.destinationViewController;
+         userdetailsObj.UpdateObjPF = self.MainMenuObjPF;
+     }
+     if ([segue.identifier isEqualToString:@"MainMenuToUserListSegue"]) {
+         
+         
+         UserList *userListObj = (UserList *)segue.destinationViewController;
+        // userdetailsObj.UpdateObjPF = self.MainMenuObjPF;
+     }
+
+     
+ }
+ 
 @end

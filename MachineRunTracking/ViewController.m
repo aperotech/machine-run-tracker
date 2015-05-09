95192c3584d9ea1b4d12906dcf7f9ffe6e4c37ff
Akshay Shrirao <ashrirao@Aperos-Mac-mini.local>
2015-05-09 17:02:41 +0530

    Added an Email Validations

--- MachineRunTracking/ViewController.m
+++ MachineRunTracking/ViewController.m
@@ -30,19 +30,38 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    if (textField.text.length >= 20 && range.length == 0)
+        return NO;
+    // Only characters in the NSCharacterSet you choose will insertable.
+        if (textField ==userEmailText) {
+        //NSString *stricterFilterString = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+        NSCharacterSet *invalidCharSet = [[NSCharacterSet characterSetWithCharactersInString:@"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_@."] invertedSet];
+        NSString *filtered = [[string componentsSeparatedByCharactersInSet:invalidCharSet] componentsJoinedByString:@""];
+        
+        return [string isEqualToString:filtered];
+    }
+    
+    return YES;
+}
+
+
+
 -(IBAction)Login:(id)sender{
     [self.activityIndicatorView startAnimating];
     NSString *email = [userEmailText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     NSString *password = [passwordText.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     
-    if ([email length] == 0 || [password length] == 0) {
+    if ([email length] == 0 || [password length] == 0)
+    {
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
                                                             message:@"You have to enter a Email and password"
                                                            delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [alertView show];
         [self.activityIndicatorView stopAnimating];
     }
-    else {
+    else
+    {
         PFQuery *query = [PFUser query];
         [query whereKey:@"email" equalTo:email];
         [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error){
